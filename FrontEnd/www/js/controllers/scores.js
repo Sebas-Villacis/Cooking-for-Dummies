08dfbe08e131f573e35a94f974fc12ef1a320441
Leo <43622237+KAII-KILLER@users.noreply.github.com>
2018-11-16 00:04:39 -0500

    Add files via upload
    Conexion

---
+++ FrontEnd/www/js/controllers/scores.js
@@ -0,0 +1,11 @@
+(function(){
+    var app = angular.module('store');
+    app.Controller('ScoreController', function($http, $scope){
+        $http.get('http://localhost:8000/api/scores').success(function(data){
+            $scope.scores = data;
+        });
+        .error(function(error){
+            debugger;
+        });
+    });
+})();
