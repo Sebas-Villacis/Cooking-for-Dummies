0e008e56cafd8aba57a342599d2ea92274882988
Leo <43622237+KAII-KILLER@users.noreply.github.com>
2018-12-14 17:25:14 -0500

    Mostrar Puntos

--- FrontEnd/src/pages/logro/logro.ts
+++ FrontEnd/src/pages/logro/logro.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController, ModalController } from 'ionic-angular';
 
 /**
  * Generated class for the LogroPage page.
@@ -15,7 +15,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LogroPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  /*constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }*/
+
+  constructor(private toastCtrl: ToastController) {
+
+  }
+
+  presentToastPuntosGanados() {
+    let toast = this.toastCtrl.create({
+      message: 'Has ganado 10 puntos!',
+      duration: 3000,
+      position: 'middle'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
+  }
+
+  presentToastPuntosPerdidos() {
+    let toast = this.toastCtrl.create({
+      message: 'Has perdido 10 puntos!',
+      duration: 3000,
+      position: 'middle'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
   }
 
   ionViewDidLoad() {
