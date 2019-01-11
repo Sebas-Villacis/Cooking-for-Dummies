030d6005b592c60058207c1dedc44b07f88a03fe
SebasV01 <sebastian.villacis01@gmail.com>
2019-01-11 14:32:16 -0500

    ajuste pantalla recetas

--- FrontEnd/src/providers/receta-servicios/receta-servicios.ts
+++ FrontEnd/src/providers/receta-servicios/receta-servicios.ts
@@ -13,7 +13,7 @@ export class RecetaServiciosProvider {
   basepath="/api";
   constructor(public http: HttpClient,private _platform:Platform) {
     if (this._platform.is("cordova")){
-      this.basepath = "http://172.16.0.96:8100/api"
+      this.basepath = "http://192.168.137.5:8100/api"
     }
   }
   getRecetas(){
